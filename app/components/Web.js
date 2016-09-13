e699fb21a662b1127bd4d13273f3dccbe567b526
Shawn <san.an.lee@gmail.com>
2016-09-14 04:08:10 +0800

    Add Webview shadow

--- app/components/Web.js
+++ app/components/Web.js
@@ -1,20 +1,33 @@
 import React, { PropTypes } from 'react';
-import { View, WebView } from 'react-native';
+import { View, WebView, StyleSheet } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import ToolBar from './ToolBar';
+
+const styles = StyleSheet.create({
+  toolbar: {
+    backgroundColor: 'white',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowRadius: 1,
+    shadowColor: 'black',
+    shadowOpacity: 0.6,
+  },
+});
 
 const Web = ({ title, uri }) => (
   <View style={{ flex: 1 }}>
     <ToolBar
       title={title}
       leftIcon="close"
-      statusbarColor="#388e3c"
-      style={{ backgroundColor: '#4caf50' }}
+      statusbarColor="#9e9e9e"
+      style={styles.toolbar}
       onLeftClicked={Actions.pop}
     />
     <WebView
       source={{ uri }}
-      style={{ }}
+      style={{ backgroundColor: "#f9f9f9", marginTop: 5 }}
     />
   </View>
 );
