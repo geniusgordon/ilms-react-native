bbdaa1ada6e89701633cf13df4036ce759c0634f
Gordon <geniusgordon@gmail.com>
2016-10-02 19:01:06 +0800

    timetable const

--- app/containers/Timetable/ClassNumber.js
+++ app/containers/Timetable/ClassNumber.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import { ScrollView, View, Text } from 'react-native';
+import { classNumber } from './const';
 import styles from './styles';
-
-const classNumber = ['1', '2', '3', '4', 'n', '5', '6', '7', '8', '9', 'a', 'b', 'c'];
 
 class ClassNumber extends Component {
   scrollViewRef = (ref) => {
--- app/containers/Timetable/index.js
+++ app/containers/Timetable/index.js
@@ -1,23 +1,6 @@
 import { connect } from 'react-redux';
 import Timetable from './Timetable';
-
-const weekday = ['M', 'T', 'W', 'R', 'F', 'S'];
-const classNumber = ['1', '2', '3', '4', 'n', '5', '6', '7', '8', '9', 'a', 'b', 'c'];
-const classTime = [
-  { from: [8, 0], to: [9, 0] },
-  { from: [9, 0], to: [10, 0] },
-  { from: [10, 0], to: [11, 10] },
-  { from: [11, 10], to: [12, 10] },
-  { from: [12, 10], to: [13, 10] },
-  { from: [13, 10], to: [14, 20] },
-  { from: [14, 20], to: [15, 20] },
-  { from: [15, 20], to: [16, 30] },
-  { from: [16, 30], to: [17, 30] },
-  { from: [17, 30], to: [18, 30] },
-  { from: [18, 30], to: [19, 30] },
-  { from: [19, 30], to: [20, 30] },
-  { from: [20, 30], to: [21, 20] },
-];
+import { weekday, classNumber, classTime } from './const';
 
 const getCourseList = ({ courseById, courseList }) => (
   courseList.current.map(id => courseById[id])
